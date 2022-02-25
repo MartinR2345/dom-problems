0b70f7f36b59745875b1685f0c96e15bcfbcf654
Martin Reece <martin@Martins-Air.home>
2022-02-25 18:24:13 -0500

    I made some changes to my HTML file. I felt like I needed to clean it up a bit

--- dom-problem2.js
+++ dom-problem2.js
@@ -1,9 +1,9 @@
-//Step 1: Create a paragraph <p> tag and <button> tag and link tag in HTML file
-//Step 2: Give the link tag an id name, called targetLink and I added youtube's website to href
-//Step 3: Give <button> tag an onclick event and an id name, called goToLink()
-//Step 4: In my Javascript file, write a function name goToLink() and inside that block of code or curly braces, I'm going to use the the getElementByID to target my "targetLink" as well as get the attribute and I save it to a variable name "myLink".  I made sure to target my p1 innerHTML which will be connected to my save variable "myLink"
+//Step 1: Create a paragraph <p> tag and a <button> tag and a link tag in HTML file
+//Step 2: Give the link tag an id name, called targetLink and I added youtube's website as the href. This href is used to identify sections within a document. 
+//Step 3: Give <button> element an onclick event and an id name, called goToLink()
+//Step 4: In my Javascript file, write a function name goToLink() and inside of the curly braces, I'm going to use the the getElementByID to target my "targetLink" from my link tag as well as get the attribute "href" of that link. Also I save it to a variable name "myLink".  I made sure to target my p1 element innerHTML which will be connected to my save variable "myLink"
 
 function goToLink() {
-    const link = document.getElementById('targetLink').getAttribute('href');
-    document.getElementById('p1').innerHTML = link;
+    const mylink = document.getElementById('targetLink').getAttribute('href');
+    document.getElementById('p1').innerHTML = mylink;
 }
